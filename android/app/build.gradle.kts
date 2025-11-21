ef80cb6dc8ee840cd620f3a02722a92a49145c8d
Neo <39899059+scotsalazar@users.noreply.github.com>
2025-11-21 17:39:18 +0800

    Update Gradle wrapper to 8.13

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 android {
